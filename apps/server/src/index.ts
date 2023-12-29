f54a1ee32411ea0b99310ed99202d13c4b86e372
xonx4l <sk797119@gmail.com>
2023-12-29 11:23:22 +0530

    update index.ts

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,12 @@
 import http from "http";
-
+import SocketServices from "./services/socket";
 async function init () {
+    const socketService = new SocketServices();
+
     const httpServer = http.createServer();
     const PORT = process.env.Port? process.env :8000;
+
+    socketService.io.attach(httpServer);
 
     httpServer.listen(PORT, () =>
     console.log(`HTTP Server started at PORT:${PORT}`)
