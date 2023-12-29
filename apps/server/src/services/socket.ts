6130c86078c62687bb9b0dc5042d29cac2512aac
xonx4l <sk797119@gmail.com>
2023-12-29 11:33:57 +0530

    update index.ts

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -6,6 +6,18 @@ class SocketService {
         console.log("Init Socket Service...");
          this._io = new Server()
     }
+
+    public initLiteners() {
+        const io = this.io;
+       io.on("connect", (socket) => {
+        console.log(`New socket Connected`, socket.id);
+
+        socket.on('event:message', async({message}: {message:string}) => {})
+         console.log("New Message Rec.", message);
+      });
+     
+    }
+
     get io() {
         return this._io;
     }
