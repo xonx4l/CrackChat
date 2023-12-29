7baf1cdb04e83e9872e557739de50d7da25af1ea
xonx4l <sk797119@gmail.com>
2023-12-29 11:17:37 +0530

    update socket.ts

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,14 @@
+import {Server} from 'socket.io'
+
+class SocketService {
+    private _io: Server;
+    constructor() {
+        console.log("Init Socket Service...");
+         this._io = new Server()
+    }
+    get io() {
+        return this._io;
+    }
+}
+
+export default SocketService;
